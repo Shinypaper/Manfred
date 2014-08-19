e38fdc5dcbbc043eed96da1e8fef939c0c256e88
chineebro <chineebro@hotmail.com>
2014-08-19 14:03:37 -0400

    fix conflicts

--- public_html/niteflite.php
+++ public_html/niteflite.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-<?php include 'header.php'; ?>
-
-
-	    <section class="portfolio_details section">
-    		<img src="assets/img/clients/niteflite-desktop.png" alt="">
-	    </section>
-
-		<section class="portfolio_blurb section">
-			<div class="container">
-				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consectetur, porro, nihil aut animi possimus iure fugit dignissimos error quibusdam eius iste voluptatibus laborum mollitia corporis. Impedit, libero quibusdam maiores.</h2>
-			</div>
-		</section>
-
-		<section class="portfolio_mobile section">
-			<div class="mobile_row">
-				<div class="mobile_images">
-					<div class="mobile_image">
-					<img src="assets/img/clients/PhilipStavroutablet.png" alt="">
-					</div>
-					<div class="mobile_image">
-						<img src="assets/img/clients/niteflite-mobile.png" alt="">
-					</div>
-=======
 <?php include('header.php'); ?>
 	
 	<div class="portfolio">
@@ -50,22 +26,11 @@
 					<div class="mobile_study_text">
 						<p>For a mobile view, it was important to maintian maximum functionality, where still retaining the overall feel of the site. To that end efforts were made to declutter the interface so that core actions were easy to determine.</p>
 					</div>
->>>>>>> origin/manfred
+
 				</div>
 			</div>
 		</section>
 
-<<<<<<< HEAD
-	    <section class="where section">
-	        <div class="portfolio">
-	            <div class="container">
-	                <div class="client">
-	                    <img src="http://placehold.it/200/202024&text=client" alt="">
-	                    <img src="http://placehold.it/200/202024&text=client" alt="">
-	                    <img src="http://placehold.it/200/202024&text=client" alt="">
-	                    <img src="http://placehold.it/200/202024&text=client" alt="">
-	                </div>
-=======
 	</div>	
 	    <section class="where section">
 		    <h1>More of our work.</h1>
@@ -77,13 +42,9 @@
 						<a class="client wow flipInY" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.png" alt=""></a>
 						<a class="client wow flipInY" data-wow-duration="1s" data-wow-delay="0.9s" href="#clientFour"><img src="assets/img/clients/phil-logo.png" alt=""></a>
 		            </div>
->>>>>>> origin/manfred
 	            </div>
 	        </div>
 	    </section>
 
-<<<<<<< HEAD
-		<?php include 'footer.php'; ?>
-=======
 <?php include('footer.php'); ?>
->>>>>>> origin/manfred
+
