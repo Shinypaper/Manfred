5d1a53c3227edecd1a4de72f8a18596973fff7c2
Manfred <manfred.headley@gmail.com>
2014-09-02 00:40:27 -0400

    mobile first

--- public_html/index.php
+++ public_html/index.php
@@ -15,34 +15,68 @@
 
 	<section class="why section" id="Why">
 		<div class="container">
-			<h2>Our Process:</h2>
-			<h3 class="wow fadeInUp" data-wow-duration="1.7s" data-wow-delay="0.2s">Designing <span class="code">mobile</span> first</h3>
-			<div class="mobile-first-images">
-				<div class="hidden-sm hidden-xs">
-					<img src="assets/img/mobile-first.png" alt="">		
+			<h2>Our Process: Design <span class="code">&amp;</span> Develop</h2>
+				<article>
+				<h3 class="wow fadeInUp" data-wow-duration="1.7s" data-wow-delay="0.2s">Designing <span class="code">mobile</span> first</h3>
+				<div class="mobile-first-images">
+					<div class="hidden-sm hidden-xs">
+						<img src="assets/img/mobile-first.png" alt="">		
+					</div>
+					<div class="visible-sm visible-xs">
+						<img src="assets/img/mobile-first-phone.png" alt="">		
+					</div>
 				</div>
-				<div class="visible-sm visible-xs">
-					<img src="assets/img/mobile-first-phone.png" alt="">		
+				<div>
+					<div class="mobiletext">
+						<h4 class="code">What is mobile first design?</h4>
+						<p>Mobile first design is exactly what it sounds like: Designing the content for mobile devices first.</p>
+					</div>
+					<div class="mobiletext">
+						<h4 class="code">Why design mobile first?</h4>
+						<p>According to the latest statistics <span class="code">more than 70% of all internet users access the web through their primarily through their phones.</span> That means roughly everyone will first see your website on a mobile device. So why design your website in a way that doesn't put those mobile users first?</p>
+					</div>
+					<div class="mobiletext">
+						<h4 class="code">How does this make my new website better?</h4>
+						<p>With mobile first design, <span class="code">the content shines</span>. Since there is less space to work with priority needs to be given to the content that matters first.</p>
+					</div>	
+					<div class="mobiletext">
+						<h4 class="code">What does it mean for bigger screens?</h4>
+						<p>Mobile first design doesn't mean desktop last design. Getting your website to look great on mobile devices for what will be almost everyones first look is very important, but that doesn't mean desktops are forgotten.</p>
+					</div>
 				</div>
-			</div>
-			<div>
-				<div class="mobiletext">
-					<h4 class="code">What is mobile first design?</h4>
-					<p>Mobile first design is exactly what it sounds like: Designing the content for mobile devices first.</p>
+			</article>
+			<article>
+				<h3 class="wow fadeInUp" data-wow-duration="1.7s" data-wow-delay="0.2s">Developing <span class="code">Responsively</span></h3>
+				<div>
+					<div class="row">
+						<div class="develop_icons"><i class="fa fa-search"></i></div>
+						<div class="develop_icons"><i class="fa fa-code"></i></div>
+						<div class="develop_icons"><i class="fa fa-database"></i></div>
+						<div class="develop_icons"><i class="fa fa-cog"></i></div>
+						<div class="develop_icons"><i class="fa fa-paper-plane-o"></i></div>
+					</div>
+					<div class="mobiletext">
+						<h4><i class="fa fa-search code"></i> Finding the project needs</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto nam numquam exercitationem inventore voluptatibus eaque quos laboriosam, consectetur in aut debitis facilis, molestiae hic ad voluptatum, ea atque. Officia, sunt..</p>
+					</div>	
+					<div class="mobiletext">
+						<h4><i class="fa fa-code code"></i> Coding out the design</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quos fugiat placeat, perspiciatis blanditiis enim, ipsam harum sint odit nam molestiae sit incidunt quaerat totam, officia sapiente sed culpa architecto..</p>
+					</div>
+					<div class="mobiletext">
+						<h4><i class="code fa fa-database"></i> Setting Up the Database and CMS</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, obcaecati dolor, harum laboriosam ex voluptas earum odit unde magnam, magni illo repudiandae molestiae ut veritatis deserunt repellendus minus. Officiis, iure!</p>
+					</div>
+					<div class="mobiletext">
+						<h4><i class="code fa fa-cog"></i> Responsive and Cross-browser testing</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima facere quidem ex animi ullam praesentium incidunt amet quis odit! Quo, molestiae beatae veritatis, voluptatem quam perspiciatis vero dolor at! Error!</p>
+					</div>	
+					<div class="mobiletext">
+						<h4><i class="code fa fa-paper-plane-o"></i> Launch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat voluptate quidem, eveniet aliquid, deserunt, accusantium atque, consequuntur iusto amet earum cumque aut. Fuga, aperiam quis pariatur, quasi quia commodi qui.</p>
+					</div>
 				</div>
-				<div class="mobiletext">
-					<h4 class="code">Why design mobile first?</h4>
-					<p>According to the latest statistics <span class="code">more than 70% of all internet users access the web through their primarily through their phones.</span> That means roughly everyone will first see your website on a mobile device. So why design your website in a way that doesn't put those mobile users first?</p>
-				</div>
-				<div class="mobiletext">
-					<h4 class="code">How does this make my new website better?</h4>
-					<p>With mobile first design, <span class="code">the content shines</span>. Since there is less space to work with priority needs to be given to the content that matters first.</p>
-				</div>	
-				<div class="mobiletext">
-					<h4 class="code">What does it mean for bigger screens?</h4>
-					<p>Mobile first design doesn't mean desktop last design. Getting your website to look great on mobile devices for what will be almost everyones first look is very important, but that doesn't mean desktops are forgotten.</p>
-				</div>
-			</div>
+			</article>
 		</div>
 	</section>
 
