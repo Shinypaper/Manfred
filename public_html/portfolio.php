c8c343219f7ae4c91e23c5f0090b55304083ff66
chineebro <chineebro@hotmail.com>
2014-09-11 18:47:42 -0400

    updates

--- public_html/portfolio.php
+++ public_html/portfolio.php
@@ -1,13 +1,5 @@
 	<section class="portfolio section" id="Where">
-<<<<<<< HEAD
-		<h3>Here's some of our work</h3>
-				<div class="clients">
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.3s" href="phil.php"><img src="assets/img/clients/phil-logo.png" alt=""></a>
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.5s" href="fallingbrook.php"><img src="assets/img/clients/falling-logo.png" alt=""></a>
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.png" alt=""></a>
-					<div class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.9s" href="#clientFour"><img src="assets/img/clients/seaton-logo.png" alt=""><figure class="overlay"><i class="fa fa-wrench"></i>Coming Soon...</figure></div>
-				</div>
-=======
+
 		<h2>Here's some of our work</h2>
 		<div class="clients">
 			<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.3s" href="phil.php"><img src="assets/img/clients/phil-logo.png" alt=""><div class="overlay"><i class="fa fa-search-plus"></i>Philip Stavrou</div></a>
@@ -15,5 +7,5 @@
 			<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.png" alt=""><div class="overlay"><i class="fa fa-search-plus"></i>Nite Flight</div></a>
 			<div class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.9s" href="#clientFour"><img src="assets/img/clients/seaton-logo.png" alt=""><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon...</div></div>
 		</div>
->>>>>>> origin/dillon
+
 	</section>
