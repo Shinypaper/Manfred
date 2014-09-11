0256d107bf6f22b7a1d6add6bb0271fffb33af46
dillon <dillon@razoredgelabs.com>
2014-09-10 22:39:26 -0400

    hover states on portfolios

--- public_html/portfolio.php
+++ public_html/portfolio.php
@@ -1,10 +1,9 @@
 	<section class="portfolio section" id="Where">
 		<h2>Here's some of our work</h2>
-				<div class="clients">
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.3s" href="phil.php"><img src="assets/img/clients/phil-logo.png" alt=""></a>
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.5s" href="fallingbrook.php"><img src="assets/img/clients/falling-logo.png" alt=""></a>
-					<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.png" alt=""></a>
-					<div class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.9s" href="#clientFour"><img src="assets/img/clients/seaton-logo.png" alt=""><figure class="overlay"><i class="fa fa-wrench"></i>Coming Soon...</figure></div>
-
-				</div>
+		<div class="clients">
+			<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.3s" href="phil.php"><img src="assets/img/clients/phil-logo.png" alt=""><div class="overlay"><i class="fa fa-search-plus"></i>Philip Stavrou</div></a>
+			<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.5s" href="fallingbrook.php"><img src="assets/img/clients/falling-logo.png" alt=""><div class="overlay"><i class="fa fa-search-plus"></i>Tonay and Claudia's</div></a>
+			<a class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.png" alt=""><div class="overlay"><i class="fa fa-search-plus"></i>Nite Flight</div></a>
+			<div class="client wow flipInX" data-wow-duration="1s" data-wow-delay="0.9s" href="#clientFour"><img src="assets/img/clients/seaton-logo.png" alt=""><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon...</div></div>
+		</div>
 	</section>
