2c77448716578e24675602f0ecffd68056fd39be
Manfred <manfred.headley@gmail.com>
2014-09-10 22:27:10 -0400

    added stuff

--- public_html/phil.php
+++ public_html/phil.php
@@ -9,7 +9,7 @@
 	    		<h1>Philip Stavrou</h1>
 	    	</div>	
 		<div class="portfolio_blurb">
-			<p>This client had an outdated site that was based on an old template. He wanted a fresh <span class="highlight">mobile friendly</span> site. Not only was a new design needed but a robust housing market backend was also integral to the success of his new website.</p>
+			<p>Phil had an outdated site that was based on an old template. He wanted a fresh <span class="highlight">mobile friendly</span> site. Not only was a new design needed but a robust housing market backend was also integral to the success of his business.</p>
 		</div>
 	
 			<div class="container">
@@ -20,16 +20,18 @@
 
 		<section class="portfolio_mobile section">
 			<div class="container">
-				<h2 class="mobile_header">Designed for the modern mobile user</h2>
+				<h2 class="mobile_header">Mobile View</h2>
+					<div class="mobile_study_text">
+						<p>For a mobile view, it was important to drive clients to the main sections of the site - Buy, Sell &amp; Neighbourhoods. We set this center stage for the mobile view, making it the </p>
+					</div>
 					<div>
 						<img src="assets/img/clients/phil-mobile.png" alt="">
 					</div>
 				
-				<div class="mobile_study_text">
-					<p>For a mobile view, it was important to maintian maximum functionality, where still retaining the overall feel of the site. To that end efforts were made to declutter the interface so that core actions were easy to determine.</p>
-				</div>
 			</div>
-			<a class="visit-site" href="http://www.nitefliteprodj.com" target="_blank">visit site</a>
+			<div>
+				<a class="visit-site" href="http://www.nitefliteprodj.com" target="_blank">visit site</a>
+			</div>
 
 		</section>
 
