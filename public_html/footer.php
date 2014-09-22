6159c17b25ec56f0c77696c95e7f34d303a4cb34
dillon <dillon@razoredgelabs.com>
2014-09-22 17:22:45 -0400

    dillon async styles load

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,26 +1,39 @@
 
-    <footer>
-        <a href="/"><img src="/assets/img/codebrewer-icon-alt.png" alt="footer logo"></a>
-        <p class="copyright">&copy; <?php echo Date(Y); ?> <span class="highlight">Code</span>Brewers</p>
-        <div class="footer_accent"></div>
-    </footer>
+        <footer>
+            <a href="/"><img src="/assets/img/codebrewer-icon-alt.png" alt="footer logo"></a>
+            <p class="copyright">&copy; <?php echo Date(Y); ?> <span class="highlight">Code</span>Brewers</p>
+            <div class="footer_accent"></div>
+        </footer>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
     
 
 
         <script src="assets/js/scripts.min.js"></script>
 
-    <script async>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-54936753-1', 'auto');
-        ga('send', 'pageview');
+            ga('create', 'UA-54936753-1', 'auto');
+            ga('send', 'pageview');
 
-    </script>
+        </script>
+
+        <script>
+          var cb = function() {
+            var l = document.createElement('link'); l.rel = 'stylesheet';
+            l.href = '/assets/css/styles.min.css';
+            var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+          };
+          var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+              webkitRequestAnimationFrame || msRequestAnimationFrame;
+          if (raf) raf(cb);
+          else window.addEventListener('load', cb);
+        </script>
+
     </body>
 </html>
