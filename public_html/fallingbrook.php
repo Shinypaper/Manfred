33edd41d19e687e89dc186d4692e40d03caf03f8
dillon <dillon@razoredgelabs.com>
2014-09-25 15:00:18 -0400

    updating link to fallingbrook site

--- public_html/fallingbrook.php
+++ public_html/fallingbrook.php
@@ -1,16 +1,16 @@
 <?php include('header.php'); ?>
-	
+
 	<div class="portfolio">
-		
+
 	<div class="container">
 	    <section class="portfolio_details section">
 	    	<div class="portfolio_header">
 	    		<h1>Tony &amp; Claudia's On Fallingbrook</h1>
-	    	</div>	
+	    	</div>
 		<div class="portfolio_blurb">
 			<p>Tony &amp; Claudia had a site that had old content locked in with no way to edit it themselves. Their old site was also not optimized for the modern mobile user of today. This fresh design really shines on mobile devices. We also recognized the need for a way for them to be able to input their new seasonal items, so Wordpress - one of the most popular content management systems - was chosen for them. This gave them exactly what they needed to make their customers happy, hungry, and coming back for more.</p>
 		</div>
-	
+
 			<div class="container">
 				<img src="assets/img/clients/fallingbrook-desktop.jpg" alt="fallingbrook desktop">
 			</div>
@@ -28,13 +28,13 @@
 					</div>
 			</div>
 			<div>
-				<div class="visit-site" href="http://www.tonyandclaudiasonfallingbrook.com" target="_blank">Launching Soon...</div>
+				<a class="visit-site" href="http://tonyandclaudiasonfallingbrook.ca" target="_blank">Visit Site</a>
 			</div>
 
 		</section>
 
-	</div>	
-	
+	</div>
+
 <?php include 'portfolio.php'; ?>
 
 <?php include('footer.php'); ?>
