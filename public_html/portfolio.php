485a5626b02a2b7c84b7454186885748482d6be2
Shinypaper <manfred.headley@gmail.com>
2014-12-18 20:30:19 -0500

    added seaton

--- public_html/portfolio.php
+++ public_html/portfolio.php
@@ -3,7 +3,10 @@
 		<div class="clients">
 			<a class="client wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s" href="phil.php"><img src="assets/img/clients/phil-logo.jpg" alt="phil screenshot"><div class="overlay"><i class="fa fa-search-plus"></i>Philip Stavrou</div></a>
 			<a class="client wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s" href="fallingbrook.php"><img src="assets/img/clients/falling-logo.jpg" alt="fallingbrook market screenshot"><div class="overlay"><i class="fa fa-search-plus"></i>Tony and Claudia's</div></a>
-			<a class="client wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.jpg" alt="nite flight screenshot"><div class="overlay"><i class="fa fa-search-plus"></i>Nite Flight</div></a>
-			<div class="client wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.9s"><img src="assets/img/clients/seaton-logo.png" alt="seaton screenshot"><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon</div></div>
+			<a class="client wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.7s" href="niteflite.php"><img src="assets/img/clients/nite-logo.jpg" alt="nite flight screenshot"><div class="overlay"><i class="fa fa-search-plus"></i>Nite Flite</div></a>
+			<a class="client wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.9s" href="seaton.php"><img src="assets/img/clients/seaton-logo.png" alt="seaton screenshot"><div class="overlay"><i class="fa fa-search-plus"></i>Seaton Property Maintenance</div></a>
+			<div class="client wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.1s"><img src="assets/img/clients/aspimed-logo.png" alt="seaton screenshot"><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon</div></div>
+			<div class="client wow fadeInDown" data-wow-duration="1s" data-wow-delay="1.3s"><img src="assets/img/clients/aquatech-logo.png" alt="seaton screenshot"><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon</div></div>
+			<div class="client wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.5s"><img src="assets/img/clients/magnus-logo.png" alt="seaton screenshot"><div class="overlay stay"><i class="fa fa-wrench"></i>Coming Soon</div></div>
 		</div>
 	</section>
---
+++ public_html/seaton.php
@@ -0,0 +1,41 @@
+<?php include('header.php'); ?>
+	
+	<div class="portfolio">
+		
+	<div class="container">
+	    <section class="portfolio_details section">
+	    	<div class="portfolio_header">
+	    		<h1>Seaton Property Maintenence</h1>
+	    	</div>	
+		<div class="portfolio_blurb">
+			<p>This client's wanted a website to start his business off on the right foot. When starting a small business it is important to have a clear definition of what your companies products or services are. This website gives a clear focus on the services provided and gives important testimonials. A good example of having a clean mobile friendly site even on a smaller budget.</p>
+		</div>
+	
+			<div class="container">
+				<img src="assets/img/clients/seaton-desktop.jpg" alt="seaton-desktop">
+			</div>
+		</section>
+	</div>
+
+		<section class="portfolio_mobile section">
+			<div class="container">
+				<h2 class="mobile_header">Mobile View</h2>
+					<article class="mobile_study_text">
+						<p>When a website is built in a way that keeps mobile users in mind it becomes very easy to make sure that all the content is translated just as cleanly and as beautifully as it would be on the desktop website. Thus we were able to keep the experience seamless on both platforms.</p>
+					</article>
+					<div>
+						<img src="assets/img/clients/seaton-mobile.png" alt="seaton-mobile">
+					</div>
+			</div>
+			<div>
+				<a class="visit-site" href="http://www.seatonpropertymaintenance.com" target="_blank">visit site</a>
+			</div>
+
+		</section>
+
+	</div>	
+
+<?php include 'portfolio.php'; ?>
+
+<?php include('footer.php'); ?>
+         
